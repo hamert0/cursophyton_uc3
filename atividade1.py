f801c3d7486602a3a001b782fd64c263850961e6
hamert0 <125414277+hamert0@users.noreply.github.com>
2025-04-25 21:32:20 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,13 @@
+nota1=int(input("digite sua nota"))
+nota2=int(input("digite sua nota"))
+nota3=int(input("digite sua nota"))
+nota4=int(input("digite sua nota"))
+
+soma= nota1+nota2
+
+media= soma / 2
+
+if media > 5.5:
+    print("aprovado")
+else:
+    print("reprovado")
