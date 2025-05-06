572ae9853d3a406f28c4ee16096c3b96bfecda83
hamert0 <125414277+hamert0@users.noreply.github.com>
2025-05-05 21:31:34 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -26,14 +26,16 @@ def jogo_da_forca():
             print("Você já tentou essa letra.")
         elif letra == palavra_secreta:
            
-            print("Parabéns! Você venceu!")     
+            print("Parabéns! Você venceu!") 
+            break    
         elif letra in palavra_secreta:
             letras_corretas.add(letra)
             print("Letra correta!")
         else:
             tentativas -= 1
             print(f"Letra incorreta! Tentativas restantes: {tentativas}")
-    
-    print(f"Game over! A palavra era: {palavra_secreta}")
+    if tentativas == 0:
+     print(f"Game over! A palavra era: {palavra_secreta}")
+      
 
 jogo_da_forca()
